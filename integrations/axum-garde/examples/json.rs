e5d2bcab5f175c039ba3f2d90eb556da7d5d7489
Altair Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-04-03 18:01:57 +0200

    new: Include `Altair-Bueno/axum-garde` into `garde` (#35)
    * new: Include `Altair-Bueno/axum-garde` into `garde`
    
    * fmt(axum-garde): Apply nightly formatting
    
    * chores: Enable `Unlicense` on deny.toml
    
    * Update integrations/axum-garde/Cargo.toml
    
    Co-authored-by: Jan Procházka <1665677+jprochazk@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jan Procházka <1665677+jprochazk@users.noreply.github.com>

---
+++ integrations/axum-garde/examples/json.rs
@@ -0,0 +1,43 @@
+//! Simple usage of using `axum_garde` for a REST API
+//!
+//! Run the example using
+//!
+//! ```sh
+//! cargo run --example json --features=json
+//! ```
+use axum::response::IntoResponse;
+use axum::routing::post;
+use axum::{Json, Router, Server};
+use axum_garde::WithValidation;
+use garde::Validate;
+use serde::{Deserialize, Serialize};
+
+// Define your valid scheme
+#[derive(Debug, Serialize, Deserialize, Validate)]
+struct Person {
+    #[garde(ascii, length(min = 3, max = 25))]
+    username: String,
+    #[garde(length(min = 15))]
+    password: String,
+}
+
+async fn insert_valid_person(
+    // Perform validation on the request payload
+    WithValidation(person): WithValidation<Json<Person>>,
+) -> impl IntoResponse {
+    println!("Inserted person on database: {person:?}");
+    Json(person.into_inner())
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/person", post(insert_valid_person))
+        // Create the application state
+        .with_state(());
+    println!("See example: http://127.0.0.1:8080/person");
+    Server::bind(&([127, 0, 0, 1], 8080).into())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
---
+++ integrations/axum-garde/tests/impl_handler.rs
@@ -0,0 +1,90 @@
+use axum::response::IntoResponse;
+use axum::routing::post;
+use axum::Router;
+use axum_garde::WithValidation;
+use axum_test::TestServer;
+use garde::Validate;
+use prost::Message;
+use rstest::*;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Serialize, Deserialize, Validate, Message, Clone)]
+struct Person {
+    #[prost(string, tag = "1")]
+    #[garde(length(min = 1))]
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Validate)]
+struct PathTuple(#[garde(length(min = 1))] pub String);
+
+#[derive(Debug, Error)]
+enum CustomRejection {
+    #[cfg_attr(feature = "json", error(transparent))]
+    #[cfg(feature = "json")]
+    JsonRejection(#[from] axum::extract::rejection::JsonRejection),
+}
+
+impl IntoResponse for CustomRejection {
+    fn into_response(self) -> axum::response::Response {
+        todo!("Mock implementation")
+    }
+}
+
+macro_rules! gen_assert_impl {
+    ($test_name:ident,$extractor:ty) => {
+        #[rstest]
+        #[tokio::test]
+        async fn $test_name() {
+            #[axum::debug_handler]
+            async fn handler(_: WithValidation<$extractor>) {}
+
+            let svc = Router::new().route("/", post(handler)).into_make_service();
+            _ = TestServer::new(svc).unwrap();
+        }
+    };
+}
+
+// Axum
+#[cfg(feature = "json")]
+gen_assert_impl!(assert_json_impl_handler, axum::extract::Json<Person>);
+gen_assert_impl!(
+    assert_extension_impl_handler,
+    axum::extract::Extension<Person>
+);
+#[cfg(feature = "form")]
+gen_assert_impl!(assert_form_impl_handler, axum::extract::Form<Person>);
+gen_assert_impl!(assert_path_impl_handler, axum::extract::Path<PathTuple>);
+#[cfg(feature = "query")]
+gen_assert_impl!(assert_query_impl_handler, axum::extract::Query<Person>);
+gen_assert_impl!(assert_state_impl_handler, axum::extract::State<()>);
+
+// Axum extra
+#[cfg(feature = "axum-extra-query")]
+gen_assert_impl!(
+    assert_extra_query_impl_handler,
+    axum_extra::extract::Query<Person>
+);
+#[cfg(all(feature = "axum-extra", feature = "json"))]
+gen_assert_impl!(
+    assert_withrejection_impl_handler,
+    axum_extra::extract::WithRejection<axum::Json<Person>, CustomRejection>
+);
+
+#[cfg(feature = "axum-extra-protobuf")]
+gen_assert_impl!(
+    assert_protobuf_impl_handler,
+    axum_extra::protobuf::Protobuf<Person>
+);
+
+// Other
+#[cfg(feature = "axum-msgpack")]
+gen_assert_impl!(assert_mgspack_impl_handler, axum_msgpack::MsgPack<Person>);
+#[cfg(feature = "axum-msgpack")]
+gen_assert_impl!(
+    assert_mgspackraw_impl_handler,
+    axum_msgpack::MsgPackRaw<Person>
+);
+#[cfg(feature = "axum-yaml")]
+gen_assert_impl!(assert_yaml_impl_handler, axum_yaml::Yaml<Person>);
