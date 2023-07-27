d4358ca39545448451070acd942b3ef0c03716d8
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-07-27 20:48:32 +0200

    test that `Option` is not required without `#[garde(required)]`

--- garde/tests/rules/option.rs
+++ garde/tests/rules/option.rs
@@ -3,15 +3,32 @@ use super::util;
 #[derive(Debug, garde::Validate)]
 struct Test<'a> {
     #[garde(required, length(min = 1))]
-    v: Option<&'a str>,
+    a: Option<&'a str>,
+    #[garde(length(min = 1))]
+    b: Option<&'a str>,
 }
 
 #[test]
 fn option_valid() {
-    util::check_ok(&[Test { v: Some("asdf") }], &())
+    util::check_ok(
+        &[Test {
+            a: Some("asdf"),
+            b: Some("asdf"),
+        }],
+        &(),
+    )
 }
 
 #[test]
 fn option_invalid() {
-    util::check_fail!(&[Test { v: Some("") }, Test { v: None }], &())
+    util::check_fail!(
+        &[
+            Test {
+                a: Some(""),
+                b: Some("")
+            },
+            Test { a: None, b: None }
+        ],
+        &()
+    )
 }
