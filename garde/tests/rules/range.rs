dfa1fed3e72ec41ac6da83f7953b931ffa296eb8
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-08-04 10:38:57 +0200

    add test for `range` where value refers to other field

--- garde/tests/rules/range.rs
+++ garde/tests/rules/range.rs
@@ -3,6 +3,8 @@ use super::util;
 struct Test<'a> {
     #[garde(range(min = 10, max = 100))]
     field: u64,
+    #[garde(range(min = 0, max = self.field))]
+    refers_to_field: u64,
     #[garde(inner(range(min = 10, max = 100)))]
     inner: &'a [u64],
 }
@@ -12,6 +14,7 @@ fn range_valid() {
     util::check_ok(
         &[Test {
             field: 50,
+            refers_to_field: 10,
             inner: &[50],
         }],
         &(),
@@ -24,10 +27,12 @@ fn range_invalid() {
         &[
             Test {
                 field: 9,
+                refers_to_field: 10,
                 inner: &[9]
             },
             Test {
                 field: 101,
+                refers_to_field: 200,
                 inner: &[101]
             }
         ],
