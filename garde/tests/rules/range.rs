990401fd669a63fe270b76e28910baadfb712820
JulesGuesnon <jules.guesnon@hetic.net>
2023-10-18 11:37:28 +0900

    fix(Range): Added test for float

--- garde/tests/rules/range.rs
+++ garde/tests/rules/range.rs
@@ -7,6 +7,8 @@ struct Test<'a> {
     refers_to_field: u64,
     #[garde(inner(range(min = 10, max = 100)))]
     inner: &'a [u64],
+    #[garde(range(min = 0., max = 100.))]
+    float_field: f32,
 }
 
 #[test]
@@ -16,6 +18,7 @@ fn range_valid() {
             field: 50,
             refers_to_field: 10,
             inner: &[50],
+            float_field: 10.,
         }],
         &(),
     )
@@ -28,12 +31,14 @@ fn range_invalid() {
             Test {
                 field: 9,
                 refers_to_field: 10,
-                inner: &[9]
+                inner: &[9],
+                float_field: -12.
             },
             Test {
                 field: 101,
                 refers_to_field: 200,
-                inner: &[101]
+                inner: &[101],
+                float_field: 1204.
             }
         ],
         &()
